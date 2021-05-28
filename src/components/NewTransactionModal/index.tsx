d02bfb7f91dcab2985bff7d32473da242e7745ca
Bruno Viveiros <bruferviveiros@gmail.com>
2021-05-28 14:26:37 -0300

    Inserindo transação na API

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import { FormEvent, useState } from 'react';
 import Modal from 'react-modal';
 
+import { api } from '../../services/api';
+
 import outcomeImg from '../../assets/outcome.svg';
 import incomeImg from '../../assets/income.svg';
 import closeImg from '../../assets/close.svg';
@@ -24,12 +26,14 @@ export function NewTransactionModal({
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    console.log({
+    const data = {
       title,
       value,
       category,
       type,
-    });
+    };
+
+    api.post('/transactions', data);
   }
 
   return (
