db3d343d0bd13edd0dce547114aa5b3971446d05
Bruno Viveiros <bruferviveiros@gmail.com>
2021-05-28 17:02:43 -0300

    Carregando transações

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Modal from 'react-modal';
 import { useState } from 'react';
 
-import { TransactionsContext } from './TransactionsContext';
+import { TransactionsProvider } from './TransactionsContext';
 
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
@@ -24,7 +24,7 @@ export function App() {
   }
 
   return (
-    <TransactionsContext.Provider value={[]}>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
 
       <Dashboard />
@@ -35,6 +35,6 @@ export function App() {
       />
 
       <GlobalStyle />
-    </TransactionsContext.Provider>
+    </TransactionsProvider>
   );
 }
